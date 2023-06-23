a8731c9317dd8c4b0c59f6a4c2c6c7f2da83bff8
Huzefa-Mustafa <huzefa.mustafa@sms-group.com>
2023-06-24 00:36:06 +0200

    updated SPARQL function

--- src/SPARQL.java
+++ src/SPARQL.java
@@ -1,6 +1,5 @@
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.*;
 
 public class SPARQL {
     /**
@@ -16,18 +15,22 @@ public class SPARQL {
         // Create a Query object from the SPARQL query string
         Query query = QueryFactory.create(queryStr);
         // Create a QueryExecution object with the query and the model
-        QueryExecution qexec = QueryExecution.create(query, model);
-
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
         try {
             // Execute the query and obtain the result set
             ResultSet rs = qexec.execSelect();
             // Process and print the results
             while (rs.hasNext()) {
                 QuerySolution soln = rs.nextSolution();
-                // Resource res = solution.getResource("x");
-                RDFNode x = soln.get("x");
+                RDFNode x = soln.get("x") ;       // Get a result variable by name.
+                RDFNode fname = soln.get("fname") ; // Get a result variable by
+                Resource r = soln.getResource("VarR") ; // Get a result variable - must be a resource
+                Literal l = soln.getLiteral("VarL") ;   // Get a result variable - must be a literal
                 if (x != null) {
                     System.out.println(x.toString());
+                    System.out.println(fname.toString());
+                    // System.out.println("Subject: "+ x + "\nResourse: " + r + "\nLiteral: " + l);
+
                 }
 
             }
@@ -46,10 +49,10 @@ public class SPARQL {
     public static void main(String[] args) {
         Model model = RDFModel.readRDFModel("data/vc-db-1.rdf");
         try {
-            RDFModel.writeModelToXML(model);
+            // RDFModel.writeModelToXML(model);
     
-            // executeSPARQLQuery(model, "SELECT ?x ?fname\r\n" + //
-            //         "WHERE {?x  <http://www.w3.org/2001/vcard-rdf/3.0#FN>  ?fname}");
+            executeSPARQLQuery(model, "SELECT ?x ?fname\r\n" + //
+                             "WHERE {?x  <http://www.w3.org/2001/vcard-rdf/3.0#FN>  ?fname}");
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
