29f4c8499652899eac4c16b7f0d0677470ab7413
Huzefa-Mustafa <huzefa.mustafa@sms-group.com>
2023-06-26 22:29:03 +0200

    added comments , added code for working with infModel

--- src/OWL.java
+++ src/OWL.java
@@ -31,7 +31,8 @@ public class OWL {
         }
         base.read( in, "RDF/XML" );
 
-        // create the reasoning model using the base
+
+        // Example to create the reasoning model using the base
         OntModel inf = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM_MICRO_RULE_INF, base );
 
         // create a dummy paper for this example
@@ -40,42 +41,45 @@ public class OWL {
         
         // // list the asserted types
         // for (Iterator<Resource> i = p1.listRDFTypes(true); i.hasNext(); ) {
-        //     SysteontM.out.println( p1.getURI() + " is asserted in class " + i.next() );
+        //     System.out.println( p1.getURI() + " is asserted in class " + i.next() );
         // }
         // // list the inferred types
         // p1 = inf.getIndividual( NS + "paper1" );
         // for (Iterator<Resource> i = p1.listRDFTypes(true); i.hasNext(); ) {
-        //     SysteontM.out.println( p1.getURI() + " is inferred to be in class " + i.next() );
+        //     System.out.println( p1.getURI() + " is inferred to be in class " + i.next() );
         // }
         return base;
     }
+    
     /**
-     * To print statements from the model
+     * Perform reasoning on the data using the provided schema.
      * 
-     * @param model Model, 
-     * @param uri String uri of the resource
-     * @return model Model
-    */
-    public static void printSubClasses(OntModel model){
-        // check the statement in model and print them
-        // list the statements in the Model
-        for ( Iterator<OntProperty> i= model.listAllOntProperties(); i.hasNext(); ) {
-                OntProperty c = i.next();
-                System.out.println(c.getURI());
-                System.out.println("###################################################");
-
-            }
-    }
-    public static void performReasoning(){
+     * @param schema Model representing the OWL schema
+     * @param data Model representing the data
+     */
+    public static void performReasoning(OntModel model){
+        // Load the schema and data models
         Model schema = RDFDataMgr.loadModel("file:src/data/owlDemoSchema.rdf");
-        Model data = RDFDataMgr.loadModel("file:src/data/owlDemoData.rdf");
+        Model data;
+        // Create and bind the reasoner to the schema
         Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-        reasoner = reasoner.bindSchema(schema);
+        if (model != null) {
+            reasoner = reasoner.bindSchema(model);
+            data = model;
+        }else{
+            reasoner = reasoner.bindSchema(schema);
+            data = RDFDataMgr.loadModel("file:src/data/owlDemoData.rdf");
+        }
+        
+        // Create an inference model using the reasoner and data
         InfModel infmodel = ModelFactory.createInfModel(reasoner, data);
         
+        // Get and print statements related to the resource "nForce"
         Resource nForce = infmodel.getResource("urn:x-hp:eg/nForce");
         System.out.println("nForce *:");
         printStatements(infmodel, nForce, null, null);
+        
+        // Check if the resource "whiteBox" is recognized as a gaming computer
         Resource gamingComputer = infmodel.getResource("urn:x-hp:eg/GamingComputer");
         Resource whiteBox = infmodel.getResource("urn:x-hp:eg/whiteBoxZX");
         if (infmodel.contains(whiteBox, RDF.type, gamingComputer)) {
@@ -83,67 +87,113 @@ public class OWL {
         } else {
             System.out.println("Failed to recognize white box correctly");
         }
-        ValidityReport validity = infmodel.validate();
+        
+        // Check the validity of the inference model
+        // checkValidity(infmodel);
+        
+    }
+    /**
+     * Check the validity of the inferred model.
+     * 
+     * @param infmodel InfModel to be checked for validity
+     */
+    public static void checkValidity(InfModel infmodel){
+        // Validate the inference model
+         ValidityReport validity = infmodel.validate();
         if (validity.isValid()) {
             System.out.println("OK");
         } else {
             System.out.println("Conflicts");
+            // Print the validity report
             for (Iterator<Report> i = validity.getReports(); i.hasNext(); ) {
                 ValidityReport.Report report = (ValidityReport.Report)i.next();
                 System.out.println(" - " + report);
             }
         }
-        
     }
+    /**
+     * To print statements from the model
+     * 
+     * @param m Model to be printed
+     * @param s Resource subject of the statement (can be null for any)
+     * @param p Property predicate of the statement (can be null for any)
+     * @param o Resource object of the statement (can be null for any)
+     */
     public static void printStatements(Model m, Resource s, Property p, Resource o) {
+        // Iterate over the statements in the model and print them
         for (StmtIterator i = m.listStatements(s,p,o); i.hasNext(); ) { 
             Statement stmt = i.nextStatement(); 
             System.out.println(" - " + PrintUtil.print(stmt)); 
         } 
     }
+    /**
+     * To print subclasses of the given class
+     * 
+     * @param ontClass OntClass to print
+     * 
+    */
+    public static void printSubClasses(OntClass ontClass){
+        for ( Iterator<OntClass> i= ontClass.listSubClasses(); i.hasNext(); ) {
+                OntClass c = i.next();
+                System.out.println(c.getURI());
+                System.out.println("###################################################");
+    
+            }
+    }
+    
+    
+    
+    
     public static void main(String[] args) {
-        // Model model = RDFModel.readRDFModel("data/vc-db-1.rdf");
-        performReasoning();
+        // performReasoning(null);
         try { 
+            
             // Load and parse the OWL ontology file
-            // String path = "src/data/eswc-2006-09-21.rdf";
-            // // create the base model
-            // String SOURCE = "http://www.eswc2006.org/technologies/ontology";
-            // String NS = SOURCE + "#";
-            // OntModel ontM = readOntModel(path, SOURCE, NS);
-            // // RDFModel.writeModelToXML(ontM);
+            String path = "src/data/eswc-2006-09-21.rdf";
+            String SOURCE = "http://www.eswc2006.org/technologies/ontology";
+            String NS = SOURCE + "#";
+            OntModel ontM = readOntModel(path, SOURCE, NS);
 
-            // OntClass cls = ontM.getOntClass(NS + "Artefact");
-            // Resource r = ontM.getResource( NS + "Artefact" );
+            // Create an OntClass for "Artefact"
+            OntClass artefact = ontM.getOntClass(NS + "Artefact");
+            // printStatements(ontM, artefact, null, null);
+            // Create a new OntClass for "Programme"
+            OntClass programme = ontM.createClass(NS + "Programme");
+
+            // Create a new OntClass for "OrganizedEvent"
+            OntClass orgEvent = ontM.createClass(NS + "OrganizedEvent");
+
+            // Create an ObjectProperty "hasProgramme"
+            ObjectProperty hasProgramme = ontM.createObjectProperty(NS + "hasProgramme");
+            hasProgramme.addDomain(orgEvent);
+            hasProgramme.addRange(programme);
+            hasProgramme.addLabel("has programme", "en");
+
+            // Create DatatypeProperties for deadlines
+            DatatypeProperty subDeadline = ontM.getDatatypeProperty(NS + "hasSubmissionDeadline");
+            DatatypeProperty notifyDeadline = ontM.getDatatypeProperty(NS + "hasNotificationDeadline");
+            DatatypeProperty cameraDeadline = ontM.getDatatypeProperty(NS + "hasCameraReadyDeadline");
+
+            // Create a DatatypeProperty "deadline" with domain "Call" and range XSD.dateTime
+            DatatypeProperty deadline = ontM.createDatatypeProperty(NS + "deadline");
+            deadline.addDomain(ontM.getOntClass(NS + "Call"));
+            deadline.addRange(XSD.dateTime);
+
+            // Connect the sub-properties to the main property "deadline"
+            deadline.addSubProperty(subDeadline);
+            deadline.addSubProperty(notifyDeadline);
+            deadline.addSubProperty(cameraDeadline);
+            // printStatements(ontM, subDeadline, null, null);
             
-            // // RDFModel.printPropertiesFromResourse(r, SOURCE);
-            
-            // OntClass programme = ontM.createClass( NS + "Programme" );
-            // OntClass orgEvent = ontM.createClass( NS + "OrganizedEvent" );
+            // Perform reasoning and inference using an OWLReasoner
+            Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+            reasoner = reasoner.bindSchema(ontM);
+            InfModel infModel = ModelFactory.createInfModel(reasoner, ontM);
 
-            // ObjectProperty hasProgramme = ontM.createObjectProperty( NS + "hasProgramme" );
-
-            // hasProgramme.addDomain( orgEvent );
-            // hasProgramme.addRange( programme );
-            // hasProgramme.addLabel( "has programme", "en" );
-
-
-            // DatatypeProperty subDeadline = ontM.getDatatypeProperty( NS + "hasSubmissionDeadline" );
-            // DatatypeProperty deadline = ontM.createDatatypeProperty( NS + "deadline" );
-            // deadline.addDomain( ontM.getOntClass( NS + "Call" ) );
-            // deadline.addRange( XSD.dateTime );
-
-            // deadline.addSubProperty( subDeadline );
-            // // printSubClasses(ontM);
-            // // SysteontM.out.println( paper );
-            // // // Perform reasoning and inference
-            // Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-            // reasoner = reasoner.bindSchema(ontM);
-            // InfModel infModel = ModelFactory.createInfModel(reasoner, ontM);
-            // Resource a = infModel.getResource(NS + "Programme");
-            // System.out.println("Statement: " + a.getProperty(subDeadline));
-            // // Perform consistency checking
-            // boolean isConsistent = infModel.validate().isValid();
+            // Retrieve a resource and print its statements
+            Resource a = infModel.getResource(NS + "hasSubmissionDeadline");
+            printStatements(infModel, a, null, null);
+            // Perform consistency checking
             
             
         } catch (Exception e) {
